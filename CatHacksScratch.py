d526aec8a4e26eebcf27be3e907f32c21b063be4
Rainstorm0 <53408757+Rainstorm0@users.noreply.github.com>
2020-12-12 13:43:01 -0500

    Update CatHacksScratch.py

--- CatHacksScratch.py
+++ CatHacksScratch.py
@@ -8,6 +8,7 @@ class html_parser(HTMLParser):
     def __init__(self):
         super().__init__()
         self.link_set = set()
+        self.print_p = False
     
     def __init__(self, link_set, print_p):
         super().__init__()
@@ -20,7 +21,7 @@ class html_parser(HTMLParser):
             if attrs[0][0] == 'href':
                 if pattern.match(attrs[0][1]):
                     self.link_set.add(attrs[0][1])
-        if tag == 'p' and print_p:
+        if tag == 'p' and self.print_p:
             print(attrs)
     
     def handle_data(self, data):
@@ -36,13 +37,13 @@ class html_parser(HTMLParser):
 def dispGuide(url):
     html_doc_raw = urllib.request.urlopen(url)
     html_doc = html_doc_raw.read().decode("utf8")
-    html_processor = html_parser(link_set=None, True)
+    html_processor = html_parser(link_set=None, print_p=True)
     html_processor.feed(html_doc)
 
 html_doc_raw = urllib.request.urlopen("https://docs.oracle.com/javase/8/docs/")
 html_doc = html_doc_raw.read().decode("utf8")
 link_set = set()
-html_processor = html_parser(link_set)
+html_processor = html_parser(link_set, False)
 html_processor.feed(html_doc)
 
 #print(link_set)
